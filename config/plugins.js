21daaeaa988fa22af32db10b2fa7558e1ce8bf0e
Jibmo <dev@poodoge.net>
2023-09-01 17:52:26 -0400

    .

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-    // ...
     upload: {
       config: {
         provider: 'local',
@@ -8,5 +7,4 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    // ...
   });

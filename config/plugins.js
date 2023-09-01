0f5fc02df6b27c8dac3180fe38e4e0503e08cfc7
Jibmo <dev@poodoge.net>
2023-09-01 18:04:12 -0400

    ..

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,36 @@
 module.exports = ({ env }) => ({
+    // ...
     upload: {
       config: {
-        provider: 'local',
+        provider: "strapi-provider-upload-ipfs-storage",
         providerOptions: {
-          sizeLimit: 100000,
+          defaultStorage: "web3",
+          filebase: {
+            // https://console.filebase.com/keys
+            key: env("FILEBASE_KEY"),
+            secret: env("FILEBASE_SECRET"),
+            bucket: env("FILEBASE_BUCKET"),
+          },
+          pinata: {
+            // https://app.pinata.cloud/keys
+            jwt: env("PINATA_JWT"),
+          },
+          fleek: {
+            // https://app.fleek.co/#/settings/general/profile
+            key: env("FLEEK_KEY"),
+            secret: env("FLEEK_SECRET"),
+            bucket: env("FLEEK_BUCKET"),
+          },
+          web3: {
+            // https://web3.storage/tokens/
+            token: env("WEB3_TOKEN"),
+          },
+          lighthouse: {
+            // https://files.lighthouse.storage/dashboard/apikey
+            token: env("LIGHTHOUSE_TOKEN"),
+          },
         },
       },
     },
+    // ...
   });
